a7586525bf529a5082d5913588d812e62de780e7
Marcos Imfeld <sebastianimfeld@gmail.com>
2020-08-21 11:57:56 -0300

    create urls file for friendgroup app | add MeetingListView

---
+++ friendgroups/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+
+from . import views
+
+
+app_name='friendgroups'
+urlpatterns = [
+    path('', views.MeetingListView.as_view(),  name='index')
+]
--- friendgroups/views.py
+++ friendgroups/views.py
@@ -1,3 +1,10 @@
+from django.views.generic.list import ListView
 from django.shortcuts import render
 
+
 # Create your views here.
+from .models import Meeting
+
+
+class MeetingListView(ListView):
+    model = Meeting
