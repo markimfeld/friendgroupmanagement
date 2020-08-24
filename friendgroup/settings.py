2361da4b9779ec6c22a4dfcf9772640e23791062
Marcos Imfeld <sebastianimfeld@gmail.com>
2020-08-24 09:40:52 -0300

    syncronized general project with accounts app

--- friendgroup/settings.py
+++ friendgroup/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'friendgroups',
     'django.contrib.admin',
     'django.contrib.auth',
--- friendgroup/urls.py
+++ friendgroup/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('friendgroups.urls'))
+    path('', include('friendgroups.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
